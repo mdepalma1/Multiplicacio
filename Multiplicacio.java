213c1b3f1d1b2011c41a1e9f98ab6429d90577be
Miriam <miriamdp78@gmail.com>
2023-12-12 19:13:24 +0100

    Agregado calculo multiplicación

---
+++ Multiplicacio.java
@@ -0,0 +1,17 @@
+package nuevoPackage;
+
+public class Multiplicacio {
+
+	public static void main(String[] args) {
+
+		int multiplicando = 2;
+		int multiplicador = 3;
+		int producto = 0;
+		 
+		producto = multiplicando * multiplicador;
+			
+		System.out.println("El resultado de la multiplicación es: " + producto);
+		
+	}
+
+}
